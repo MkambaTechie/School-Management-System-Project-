4a20b6dd183fe8df3c65062540f05f083e054744
Athuman Mkamba <112406609+MkambaTechie@users.noreply.github.com>
2025-09-06 11:50:17 +0300

    Update page.tsx

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -17,7 +17,7 @@ const SingleStudentpage = () => {
 
             <div className="w-2/3 flex flex-col justify-between gap-4">
             <h1 className="text-xl font-semibold">Oh! My Son (Ally)</h1>
-            <p className="text-sm text-gray-500">It's going to be scary but not for us</p> 
+            <p className="text-sm text-gray-500">It is going to be scary but not for us</p> 
             <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                     <Image src="/blood.png" alt="" width={14} height={14} />
